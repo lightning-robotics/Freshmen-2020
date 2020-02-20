16fafa38ae3234572058c7a29909e3a57e99786e
kaiho <kaihoshijo003@gmail.com>
2020-02-19 20:52:51 -0700

    Method for calculating the formula that would shoot the ball a certain distance

--- src/main/java/frc/robot/commands/Shooter/ShooterGoalOfTheDay.java
+++ src/main/java/frc/robot/commands/Shooter/ShooterGoalOfTheDay.java
@@ -5,20 +5,13 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands.Mechanisms;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+package frc.robot.commands.Shooter;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 import frc.robot.Robot;
-import frc.robot.RobotMap;
 
 public class ShooterGoalOfTheDay extends Command {
-
-  TalonSRX shooter1 = new TalonSRX(RobotMap.SHOOTER_TOP); //top
-  TalonSRX shooter2 = new TalonSRX(RobotMap.SHOOTER_BOTTOM);
 
   public ShooterGoalOfTheDay() {
     // Use requires() here to declare subsystem dependencies
@@ -28,19 +21,20 @@ public class ShooterGoalOfTheDay extends Command {
   // Called just before this Command runs the first time
   @Override
   public void initialize() {
-    shooter1.set(ControlMode.PercentOutput, 0);
-    shooter2.set(ControlMode.PercentOutput, 0);
+    Robot.shooter.stopShooter();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   public void execute() {
 
+    double distance = Robot.limelight.getDistanceFrom();
+
+    // TODO: change this to mechanism joystick once equations are found
     if (Robot.oi.driver.getXButton()) {
-      shooter1.set(ControlMode.PercentOutput, .75);
-      shooter2.set(ControlMode.PercentOutput, -.70);
+      Robot.shooter.shootDistance(distance);
     } else {
-      end();
+      initialize();
     }
 
   }
