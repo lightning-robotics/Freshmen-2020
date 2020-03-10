d3af13e3d83d5d92ec53c967fa0ae047ff428fea
Lightning Robotics <legacy.lightningrobotics@gmail.com>
2020-03-10 15:28:49 -0600

    changed elevator controls

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems;
 
+// import com.kauailabs.*;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.networktables.NetworkTable;
--- src/main/java/frc/robot/subsystems/Music.java
+++ src/main/java/frc/robot/subsystems/Music.java
@@ -8,7 +8,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.music.Orchestra;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
